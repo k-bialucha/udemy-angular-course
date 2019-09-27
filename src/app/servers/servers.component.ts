9bb279304dbece83cefd1b5df5bab59c90644ffd
Kamil Bialucha <kbialucha@pgs-soft.com>
2019-09-27 11:23:11 +0200

    L19: Servers component template updated to be inline

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,14 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.scss']
+  template: `
+    <p>
+      <app-server></app-server>
+      <app-server></app-server>
+    </p>
+  `,
+  styleUrls: ['./servers.component.scss'],
 })
 export class ServersComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
