188f20874628847e7d7824c1ff0ee47ad2c5f8bd
Kamil Bialucha <kbialucha@pgs-soft.com>
2019-09-30 10:55:41 +0200

    L31: Two-way databinding added

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewSever: boolean = false;
   serverCreationStatus: string = 'No server created.';
-  serverName: string = '';
+  serverName: string = 'fancy name';
 
   constructor() {
     setTimeout(() => {
