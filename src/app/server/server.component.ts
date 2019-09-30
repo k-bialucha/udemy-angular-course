b6ee5496e16096e49e8c1da4998cc114ef3e23b5
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-09-30 14:12:16 +0200

    L37: Server component statuses enhanced with ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 
+enum ServerStatus {
+  OFFLINE = 'offline',
+  ONLINE = 'online',
+}
+
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
 })
 export class ServerComponent {
   serverId: number = 11;
-  serverStatus: string = 'offline';
+  serverStatus: ServerStatus = ServerStatus.ONLINE;
+
+  constructor() {
+    this.serverStatus =
+      Math.random() > 0.5 ? ServerStatus.ONLINE : ServerStatus.OFFLINE;
+  }
 
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor(): string {
+    if (this.serverStatus === ServerStatus.ONLINE) return 'green';
+
+    return 'red';
   }
 }
