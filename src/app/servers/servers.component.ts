cd4a61f162709fdf185b8d92788f6f133a3b4ce4
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-09-30 12:28:08 +0200

    L32: onCreateServer method state change updated

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+interface ComponentState {
+  allowNewServer: boolean;
+  serverCreationStatus: string;
+  serverName: string;
+}
+
+const initialState: ComponentState = {
+  allowNewServer: false,
+  serverCreationStatus: 'waiting for server to be added...',
+  serverName: 'my server',
+};
+
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.scss'],
 })
 export class ServersComponent implements OnInit {
-  allowNewSever: boolean = false;
-  serverCreationStatus: string = 'No server created.';
-  serverName: string = 'fancy name';
+  allowNewSever: boolean = initialState.allowNewServer;
+  serverCreationStatus: string = initialState.serverCreationStatus;
+  serverName: string = initialState.serverName;
 
   constructor() {
     setTimeout(() => {
@@ -19,7 +31,14 @@ export class ServersComponent implements OnInit {
   ngOnInit() {}
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus = `Server >>${this.serverName}<< was created!`;
+    this.allowNewSever = false;
+
+    setTimeout(() => {
+      this.allowNewSever = true;
+      this.serverCreationStatus = initialState.serverCreationStatus;
+      this.serverName = initialState.serverName;
+    }, 2000);
   }
 
   onUpdateServerName(event: Event) {
