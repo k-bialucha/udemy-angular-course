7e46efe8d226eca9bf38e75f1202f5ad96d4a189
Kamil Bialucha <kbialucha@pgs-soft.com>
2019-09-30 11:35:43 +0200

    A1: SuccessAlert component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { WarningAlertComponent } from './warning-alert/warning-alert.component';
     ServerComponent,
     ServersComponent,
     WarningAlertComponent,
+    SuccessAlertComponent,
   ],
   imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  templateUrl: './success-alert.component.html',
+  styleUrls: ['./success-alert.component.scss'],
+})
+export class SuccessAlertComponent implements OnInit {
+  message: string = 'operation was very successful!';
+  constructor() {}
+
+  ngOnInit() {}
+}
