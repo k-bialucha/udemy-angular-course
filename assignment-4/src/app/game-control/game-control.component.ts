cf49e8617c90afbd353dad337ecc0f6ee4a4bf27
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-02 12:03:39 +0200

    A4: event emitter configured in the GameControl component

--- assignment-4/src/app/game-control/game-control.component.ts
+++ assignment-4/src/app/game-control/game-control.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+const EMIT_INTERVAL = 300;
 
 @Component({
   selector: 'app-game-control',
   templateUrl: './game-control.component.html',
-  styleUrls: ['./game-control.component.css']
+  styleUrls: ['./game-control.component.css'],
 })
 export class GameControlComponent implements OnInit {
+  @Output('newNumberEmitted') numberEmitter: EventEmitter<
+    number
+  > = new EventEmitter();
+  currentNumber: number = 0;
+  intervalId: number = null;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onStart() {
+    this.emitCurrentNumber();
+
+    this.intervalId = window.setInterval(() => {
+      this.currentNumber++;
+      this.emitCurrentNumber();
+    }, EMIT_INTERVAL);
   }
 
+  onStop() {
+    window.clearInterval(this.intervalId);
+    this.intervalId = null;
+    this.currentNumber = 0;
+  }
+
+  emitCurrentNumber() {
+    this.numberEmitter.emit(this.currentNumber);
+  }
 }
