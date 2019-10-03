dd8a1aae70d5b94be38aff43d6287215e8ac9926
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-03 18:11:02 +0200

    L110: Logging service usage removed from components

--- section-9/src/app/new-account/new-account.component.ts
+++ section-9/src/app/new-account/new-account.component.ts
@@ -1,23 +1,21 @@
 import { Component } from '@angular/core';
 
 import { AccountsService } from '../accounts.service';
-import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService],
 })
 export class NewAccountComponent {
   constructor(
-    private accountsService: AccountsService,
-    private loggingService: LoggingService
-  ) {}
+    private accountsService: AccountsService
+  ) // private loggingService: LoggingService
+  {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
 
-    this.loggingService.logStatusChange(accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
   }
 }
