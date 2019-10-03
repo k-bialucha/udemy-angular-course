ea17f0d6283c85c2c75ad102de0e40b3a570b19c
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-03 11:01:28 +0200

    L97: input properties added to the BetterHighlight directive

--- section-7/src/app/better-highlight/better-highlight.directive.ts
+++ section-7/src/app/better-highlight/better-highlight.directive.ts
@@ -5,12 +5,16 @@ import {
   ElementRef,
   HostListener,
   HostBinding,
+  Input,
 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]',
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'orange';
+
   @HostBinding('style.backgroundColor') backgroundColor: string;
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
@@ -18,7 +22,7 @@ export class BetterHighlightDirective implements OnInit {
   ngOnInit(): void {}
 
   @HostListener('mouseenter') mouseover(eventData: Event): void {
-    this.backgroundColor = 'orange';
+    this.backgroundColor = this.highlightColor;
     // this.renderer.setStyle(
     //   this.elementRef.nativeElement,
     //   'background-color',
@@ -26,7 +30,7 @@ export class BetterHighlightDirective implements OnInit {
     // );
   }
   @HostListener('mouseleave') mouseleave(eventData: Event): void {
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
     // this.renderer.setStyle(
     //   this.elementRef.nativeElement,
     //   'background-color',
