d168a057a12f5d9a7593e50049541dc5e851d9a1
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-03 10:52:46 +0200

    L96: method of accessing the CSS property updated

--- section-7/src/app/better-highlight/better-highlight.directive.ts
+++ section-7/src/app/better-highlight/better-highlight.directive.ts
@@ -4,28 +4,33 @@ import {
   OnInit,
   ElementRef,
   HostListener,
+  HostBinding,
 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]',
 })
 export class BetterHighlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor: string;
+
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit(): void {}
 
   @HostListener('mouseenter') mouseover(eventData: Event): void {
-    this.renderer.setStyle(
-      this.elementRef.nativeElement,
-      'background-color',
-      'blue'
-    );
+    this.backgroundColor = 'orange';
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   'background-color',
+    //   'blue'
+    // );
   }
   @HostListener('mouseleave') mouseleave(eventData: Event): void {
-    this.renderer.setStyle(
-      this.elementRef.nativeElement,
-      'background-color',
-      'transparent'
-    );
+    this.backgroundColor = 'transparent';
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   'background-color',
+    //   'transparent'
+    // );
   }
 }
