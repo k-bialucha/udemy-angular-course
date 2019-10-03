8a383d89f278f4bb72afe83c05e963d9af5fc080
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-03 12:07:22 +0200

    L100: ngSwitch example added

--- section-7/src/app/app.component.ts
+++ section-7/src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   // numbers = [1, 2, 3, 4, 5];
   oddNumbers = [1, 3, 5];
   evenNumbers = [2, 4, 6];
 
   onlyOdd = false;
+
+  switchValue = 1;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.switchValue = 10;
+    }, 3000);
+    setTimeout(() => {
+      this.switchValue = 999;
+    }, 5000);
+  }
 }
