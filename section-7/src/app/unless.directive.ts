11ac13e387bad040e61343237486ff61ab2461a0
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-03 11:58:39 +0200

    L99: Unless structural directive implemented

---
+++ section-7/src/app/unless.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, Input, ViewContainerRef, TemplateRef } from '@angular/core';
+
+@Directive({
+  selector: '[appUnless]',
+})
+export class UnlessDirective {
+  @Input() set appUnless(condition: boolean) {
+    if (!condition) {
+      this.viewContainerRef.createEmbeddedView(this.templateRef);
+    } else {
+      this.viewContainerRef.clear();
+    }
+  }
+  constructor(
+    private templateRef: TemplateRef<any>,
+    private viewContainerRef: ViewContainerRef
+  ) {}
+}
