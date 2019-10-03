25b1de6e4311a2a59af1dc08b78c549725515676
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-03 10:33:21 +0200

    L93: BasicHighlight directive created

--- section-7/src/app/app.module.ts
+++ section-7/src/app/app.module.ts
@@ -4,15 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
+  declarations: [AppComponent, BasicHighlightDirective],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
