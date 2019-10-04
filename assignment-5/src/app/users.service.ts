99c0517b3ed657694876d50d3e6c76011c29a525
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-04 13:34:49 +0200

    A5: unnecessary event emitter removed from the Users service

--- assignment-5/src/app/users.service.ts
+++ assignment-5/src/app/users.service.ts
@@ -8,21 +8,14 @@ export class UsersService {
   activeUsers: UserList = ['Max', 'Anna'];
   inactiveUsers: UserList = ['Chris', 'Manu'];
 
-  private usersUpdateEmitter: EventEmitter<void> = new EventEmitter<void>();
-
   public subscribeToUpdates: (handler: () => any) => void;
 
-  constructor(private counterService: UpdateCounterService) {
-    this.subscribeToUpdates = handler => {
-      this.usersUpdateEmitter.subscribe(handler);
-    };
-  }
+  constructor(private counterService: UpdateCounterService) {}
 
   setActiveToInactive(id: number) {
     this.inactiveUsers.push(this.activeUsers[id]);
     this.activeUsers.splice(id, 1);
 
-    this.notifyAfterUpdate();
     this.counterService.incrementActiveToInactiveCount();
   }
 
@@ -30,11 +23,6 @@ export class UsersService {
     this.activeUsers.push(this.inactiveUsers[id]);
     this.inactiveUsers.splice(id, 1);
 
-    this.notifyAfterUpdate();
     this.counterService.incrementInactiveToActiveCount();
-  }
-
-  private notifyAfterUpdate() {
-    this.usersUpdateEmitter.emit();
   }
 }
