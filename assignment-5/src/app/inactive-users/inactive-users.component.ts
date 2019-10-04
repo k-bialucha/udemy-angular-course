5f259fc3a9a501e899cc0588139158a2515e79d1
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-04 12:21:33 +0200

    A5: ActiveUsers and InactiveUsers components updated to use Users service

--- assignment-5/src/app/inactive-users/inactive-users.component.ts
+++ assignment-5/src/app/inactive-users/inactive-users.component.ts
@@ -1,15 +1,17 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-inactive-users',
   templateUrl: './inactive-users.component.html',
-  styleUrls: ['./inactive-users.component.css']
+  styleUrls: ['./inactive-users.component.css'],
 })
 export class InactiveUsersComponent {
   @Input() users: string[];
-  @Output() userSetToActive = new EventEmitter<number>();
+
+  constructor(private usersService: UsersService) {}
 
   onSetToActive(id: number) {
-    this.userSetToActive.emit(id);
+    this.usersService.setInactiveToActive(id);
   }
 }
