53f0407a7272ff96e44be958d61c621a75330426
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-07 19:15:54 +0200

    L175: subscription cancelled in the App component

--- section-13/src/app/app.component.ts
+++ section-13/src/app/app.component.ts
@@ -1,19 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { UserService } from './user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   isActivated = false;
+  private _subscription: Subscription;
 
   constructor(private userService: UserService) {}
 
   ngOnInit() {
-    this.userService.activatedSubject.subscribe(isActivated => {
-      this.isActivated = isActivated;
-    });
+    this._subscription = this.userService.activatedSubject.subscribe(
+      isActivated => {
+        this.isActivated = isActivated;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this._subscription.unsubscribe();
   }
 }
