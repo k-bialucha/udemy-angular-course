de744024b1666d8be5399b04d9842da47834bf60
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-07 19:13:07 +0200

    L175: basic example of the rxjs Subscriber added

--- section-13/src/app/app.component.ts
+++ section-13/src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  constructor() {}
+  isActivated = false;
 
-  ngOnInit() {}
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.activatedSubject.subscribe(isActivated => {
+      this.isActivated = isActivated;
+    });
+  }
 }
