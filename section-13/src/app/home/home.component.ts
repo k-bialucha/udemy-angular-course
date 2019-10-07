713cb9ae4972802c5fc266d109c483e22fc5c00e
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-07 17:56:07 +0200

    L170: custom observable subscription example added

--- section-13/src/app/home/home.component.ts
+++ section-13/src/app/home/home.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
+export class HomeComponent implements OnInit, OnDestroy {
+  private observableSubscription: Subscription;
+  constructor() {}
 
   ngOnInit() {
+    this.observableSubscription = interval(1000).subscribe(count => {
+      console.log('interval - count:', count);
+    });
   }
 
+  ngOnDestroy(): void {
+    console.warn('will unsubscribe:', this.observableSubscription);
+    this.observableSubscription.unsubscribe();
+  }
 }
