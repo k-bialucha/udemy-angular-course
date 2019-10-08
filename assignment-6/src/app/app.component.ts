1d0d731710c69603792795b073b80e2f457bd81e
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-08 11:52:32 +0200

    A6: basic form skeleton added

--- assignment-6/src/app/app.component.ts
+++ assignment-6/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild('loginForm', { static: true }) loginForm: NgForm;
 
+  ngOnInit(): void {
+    console.warn('INIT:', this.loginForm);
+  }
 }
