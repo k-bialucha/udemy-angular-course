05509c6b23208b23d240d1eec1a321c9cbce7455
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-09 11:29:53 +0200

    L206: nested form group created

--- section-15-reactive-forms/src/app/app.component.ts
+++ section-15-reactive-forms/src/app/app.component.ts
@@ -12,8 +12,10 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
-      username: new FormControl(null, Validators.required),
-      email: new FormControl(null, [Validators.required, Validators.email]),
+      user: new FormGroup({
+        username: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+      }),
       gender: new FormControl('male'),
     });
   }
