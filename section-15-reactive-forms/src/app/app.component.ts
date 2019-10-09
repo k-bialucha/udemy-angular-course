61b7e5696368d0fc7581598c33feb2d5f2e95a24
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-09 11:49:30 +0200

    L208: FormArray example added

--- section-15-reactive-forms/src/app/app.component.ts
+++ section-15-reactive-forms/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +10,10 @@ export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
 
+  get hobbyControls() {
+    return (this.signupForm.get('hobbies') as FormArray).controls;
+  }
+
   ngOnInit(): void {
     this.signupForm = new FormGroup({
       user: new FormGroup({
@@ -17,10 +21,16 @@ export class AppComponent implements OnInit {
         email: new FormControl(null, [Validators.required, Validators.email]),
       }),
       gender: new FormControl('male'),
+      hobbies: new FormArray([]),
     });
   }
 
   onSubmit() {
     console.log('onSubmit:', this.signupForm.value);
+  }
+
+  addHobby() {
+    const newHobby = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(newHobby);
   }
 }
