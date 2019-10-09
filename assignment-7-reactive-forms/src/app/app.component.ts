556e0d0fb80e33c1d4100e197d45bbdc84b07435
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-09 15:20:36 +0200

    A7: basic form set up

--- assignment-7-reactive-forms/src/app/app.component.ts
+++ assignment-7-reactive-forms/src/app/app.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  form: FormGroup;
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      projectName: new FormControl(''),
+    });
+
+    this.form.valueChanges.subscribe(value => {
+      console.log('form value update', value);
+    });
+  }
 }
