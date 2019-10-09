03acdbe4513257efb4d7255af3c84eee3c64f9fa
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-09 12:34:06 +0200

    L209: isForbiddenUsername validator added

--- section-15-reactive-forms/src/app/app.component.ts
+++ section-15-reactive-forms/src/app/app.component.ts
@@ -9,6 +9,7 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Kamil', 'admin', 'superuser'];
 
   get hobbyControls() {
     return (this.signupForm.get('hobbies') as FormArray).controls;
@@ -17,7 +18,10 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.signupForm = new FormGroup({
       user: new FormGroup({
-        username: new FormControl(null, Validators.required),
+        username: new FormControl(null, [
+          Validators.required,
+          this.isForbiddenUsername.bind(this),
+        ]),
         email: new FormControl(null, [Validators.required, Validators.email]),
       }),
       gender: new FormControl('male'),
@@ -32,5 +36,28 @@ export class AppComponent implements OnInit {
   addHobby() {
     const newHobby = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(newHobby);
+  }
+
+  isForbiddenUsername(control: FormControl): { [s: string]: boolean } {
+    const nameLowercase = (<string>control.value || '').toLowerCase();
+
+    const forbiddenNameFound = this.forbiddenUsernames.reduce(
+      (nameFound: boolean, currentName: string) => {
+        if (nameFound) return true;
+
+        const isCurrentNameForbidden =
+          nameLowercase === currentName.toLowerCase();
+
+        return isCurrentNameForbidden;
+      },
+      false
+    );
+
+    if (forbiddenNameFound)
+      return {
+        nameIsForbidden: true,
+      };
+
+    return null;
   }
 }
