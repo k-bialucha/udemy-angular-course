1ffe317d8af9d98448297653217564a16feebd21
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-09 15:40:27 +0200

    A7: dropdown field added

--- assignment-7-reactive-forms/src/app/app.component.ts
+++ assignment-7-reactive-forms/src/app/app.component.ts
@@ -8,11 +8,13 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   form: FormGroup;
+  statuses = ['stable', 'critical', 'finished'];
 
   ngOnInit() {
     this.form = new FormGroup({
       projectName: new FormControl('', Validators.required),
       email: new FormControl('', [Validators.required, Validators.email]),
+      projectStatus: new FormControl(this.statuses[1]),
     });
 
     this.form.valueChanges.subscribe(value => {
