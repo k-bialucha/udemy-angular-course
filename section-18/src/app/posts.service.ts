febe3d07fd9e46e077dcca956cd09959bf93c2fd
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-15 15:16:27 +0200

    L260: API access methods implemented in the Posts service

---
+++ section-18/src/app/posts.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+import { Post } from './post.model';
+import { Observable } from 'rxjs';
+
+const API_URL = 'https://angular-udemy-course-backend.firebaseio.com';
+const POSTS_PATH = 'posts.json';
+
+const ENDPOINT_URL = `${API_URL}/${POSTS_PATH}`;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PostsService {
+  // private _isFetching: boolean = false;
+  private _loadedPosts: Post[] = [];
+
+  public get loadedPosts(): Post[] {
+    return this._loadedPosts;
+  }
+  // public get isFetching(): boolean {
+  //   return this._isFetching;
+  // }
+
+  constructor(private httpClient: HttpClient) {}
+
+  createPost(title: string, content: string): void {
+    const post = new Post(title, content);
+
+    console.warn('new post is:', post);
+
+    this.httpClient
+      .post<{ name: string }>(ENDPOINT_URL, post)
+      .subscribe(response => {
+        console.warn('POST response', response);
+      });
+  }
+
+  fetchPosts(): Observable<Post[]> {
+    return this.httpClient.get<{ [key: string]: Post }>(ENDPOINT_URL).pipe(
+      map(responseData => {
+        const keys = Object.keys(responseData);
+
+        const objectList = keys.map((key: string) => ({
+          id: key,
+          ...responseData[key],
+        }));
+        return objectList;
+      })
+    );
+  }
+}
