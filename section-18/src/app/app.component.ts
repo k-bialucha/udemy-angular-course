5efbe9d607cccd84c7175e4da0ca8647c9474902
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-15 13:08:48 +0200

    L254: initial data fetching with GET method handled

--- section-18/src/app/app.component.ts
+++ section-18/src/app/app.component.ts
@@ -16,7 +16,9 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
@@ -27,9 +29,16 @@ export class AppComponent implements OnInit {
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http.get(ENDPOINT_URL).subscribe(response => {
+      console.warn('GET posts response', response);
+    });
   }
 }
