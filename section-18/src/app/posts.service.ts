64c1d2dbe034aee0e2380207ca72cce6f9c58c67
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-16 15:43:09 +0200

    L268: observed data alternatives examples provided

--- section-18/src/app/posts.service.ts
+++ section-18/src/app/posts.service.ts
@@ -4,10 +4,12 @@ import {
   HttpErrorResponse,
   HttpHeaders,
   HttpParams,
+  HttpEvent,
+  HttpEventType,
 } from '@angular/common/http';
 
 import { Observable, Subject, throwError } from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
+import { map, catchError, tap } from 'rxjs/operators';
 
 import { Post } from './post.model';
 
@@ -45,14 +47,25 @@ export class PostsService {
 
     const post = new Post(title, content);
 
-    this.httpClient.post<{ name: string }>(ENDPOINT_URL, post).subscribe(
-      response => {
-        console.warn('POST response', response);
-      },
-      () => {
-        this.addPostError = 'Some POST error happened.';
-      }
-    );
+    this.httpClient
+      .post<{ name: string }>(ENDPOINT_URL, post, {
+        // default:
+        // observe: 'body',
+        observe: 'response',
+      })
+      .subscribe(
+        response => {
+          console.warn(
+            'POST response:',
+            response.body,
+            response.status,
+            response.url
+          );
+        },
+        () => {
+          this.addPostError = 'Some POST error happened.';
+        }
+      );
   }
 
   fetchPosts(): Observable<Post[]> {
@@ -85,7 +98,19 @@ export class PostsService {
       );
   }
 
-  clearPosts(): Observable<void> {
-    return this.httpClient.delete<void>(ENDPOINT_URL);
+  clearPosts(): Observable<HttpEvent<void>> {
+    return this.httpClient
+      .delete<void>(ENDPOINT_URL, { observe: 'events' })
+      .pipe(
+        tap(event => {
+          if (event.type === HttpEventType.Sent) {
+            console.log('sent event!', event);
+          }
+          if (event.type === HttpEventType.Response) {
+            const eventStatusText = event.statusText;
+            console.log('response event!', event, eventStatusText);
+          }
+        })
+      );
   }
 }
